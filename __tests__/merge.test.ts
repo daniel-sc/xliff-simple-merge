32354e8f844abd0a4434deab5de74843bcfe9f51
Daniel Schreiber <daniel-schreiber@gmx.de>
2022-01-20 21:55:53 +0100

    retain all nodes of translation unit - fixes #2

--- __tests__/merge.test.ts
+++ __tests__/merge.test.ts
@@ -1,4 +1,5 @@
 import {merge} from '../src/merge';
+import {XmlDocument} from 'xmldoc';
 
 describe('merge', () => {
     test('should add missing node', () => {
@@ -29,7 +30,7 @@ describe('merge', () => {
 
         const result = merge(sourceFileContent, destFileContent);
 
-        expect(result).toEqual('<xliff version="2.0" xmlns="urn:oasis:names:tc:xliff:document:2.0" srcLang="de" trgLang="fr-CH">\n' +
+        expect(norm(result)).toEqual(norm('<xliff version="2.0" xmlns="urn:oasis:names:tc:xliff:document:2.0" srcLang="de" trgLang="fr-CH">\n' +
             '  <file original="ng.template" id="ngi18n">\n' +
             '    <unit id="ID1">\n' +
             '      <segment state="translated">\n' +
@@ -43,7 +44,7 @@ describe('merge', () => {
             '        <target>source val2</target>\n' +
             '      </segment>\n' +
             '    </unit></file>\n' +
-            '</xliff>');
+            '</xliff>'));
     });
 
     test('should remove obsolete node', () => {
@@ -75,7 +76,7 @@ describe('merge', () => {
 
         const result = merge(sourceFileContent, destFileContent);
 
-        expect(result).toEqual('<xliff version="2.0" xmlns="urn:oasis:names:tc:xliff:document:2.0" srcLang="de" trgLang="fr-CH">\n' +
+        expect(norm(result)).toEqual(norm('<xliff version="2.0" xmlns="urn:oasis:names:tc:xliff:document:2.0" srcLang="de" trgLang="fr-CH">\n' +
             '  <file original="ng.template" id="ngi18n">\n' +
             '    <unit id="ID1">\n' +
             '      <segment state="translated">\n' +
@@ -84,7 +85,7 @@ describe('merge', () => {
             '      </segment>\n' +
             '    </unit>\n' +
             '  </file>\n' +
-            '</xliff>');
+            '</xliff>'));
     });
 
     test('should update changed node', () => {
@@ -110,7 +111,7 @@ describe('merge', () => {
 
         const result = merge(sourceFileContent, destFileContent);
 
-        expect(result).toEqual('<xliff version="2.0" xmlns="urn:oasis:names:tc:xliff:document:2.0" srcLang="de" trgLang="fr-CH">\n' +
+        expect(norm(result)).toEqual(norm('<xliff version="2.0" xmlns="urn:oasis:names:tc:xliff:document:2.0" srcLang="de" trgLang="fr-CH">\n' +
             '  <file original="ng.template" id="ngi18n">\n' +
             '    <unit id="ID1">\n' +
             '      <segment state="initial">\n' +
@@ -119,7 +120,7 @@ describe('merge', () => {
             '      </segment>\n' +
             '    </unit>\n' +
             '  </file>\n' +
-            '</xliff>');
+            '</xliff>'));
     });
 
     test('should fuzzy match changed node', () => {
@@ -145,7 +146,7 @@ describe('merge', () => {
 
         const result = merge(sourceFileContent, destFileContent);
 
-        expect(result).toEqual('<xliff version="2.0" xmlns="urn:oasis:names:tc:xliff:document:2.0" srcLang="de" trgLang="fr-CH">\n' +
+        expect(norm(result)).toEqual(norm('<xliff version="2.0" xmlns="urn:oasis:names:tc:xliff:document:2.0" srcLang="de" trgLang="fr-CH">\n' +
             '  <file original="ng.template" id="ngi18n">\n' +
             '    <unit id="ID1">\n' +
             '      <segment state="initial">\n' +
@@ -154,7 +155,7 @@ describe('merge', () => {
             '      </segment>\n' +
             '    </unit>\n' +
             '  </file>\n' +
-            '</xliff>');
+            '</xliff>'));
     });
 
     test('should ignore whitespace changes', () => {
@@ -180,7 +181,7 @@ describe('merge', () => {
 
         const result = merge(sourceFileContent, destFileContent);
 
-        expect(result).toEqual('<xliff version="2.0" xmlns="urn:oasis:names:tc:xliff:document:2.0" srcLang="de" trgLang="fr-CH">\n' +
+        expect(norm(result)).toEqual(norm('<xliff version="2.0" xmlns="urn:oasis:names:tc:xliff:document:2.0" srcLang="de" trgLang="fr-CH">\n' +
             '  <file original="ng.template" id="ngi18n">\n' +
             '    <unit id="ID1">\n' +
             '      <segment state="translated">\n' +
@@ -189,7 +190,7 @@ describe('merge', () => {
             '      </segment>\n' +
             '    </unit>\n' +
             '  </file>\n' +
-            '</xliff>');
+            '</xliff>'));
     });
 
     test('should not ignore whitespace changes with option collapseWhitespace=false', () => {
@@ -215,7 +216,7 @@ describe('merge', () => {
 
         const result = merge(sourceFileContent, destFileContent, {collapseWhitespace: false});
 
-        expect(result).toEqual('<xliff version="2.0" xmlns="urn:oasis:names:tc:xliff:document:2.0" srcLang="de" trgLang="fr-CH">\n' +
+        expect(norm(result)).toEqual(norm('<xliff version="2.0" xmlns="urn:oasis:names:tc:xliff:document:2.0" srcLang="de" trgLang="fr-CH">\n' +
             '  <file original="ng.template" id="ngi18n">\n' +
             '    <unit id="ID1">\n' +
             '      <segment state="initial">\n' +
@@ -224,6 +225,44 @@ describe('merge', () => {
             '      </segment>\n' +
             '    </unit>\n' +
             '  </file>\n' +
-            '</xliff>');
+            '</xliff>'));
+    });
+    test('should retain notes', () => {
+        const sourceFileContent = '<xliff version="2.0" xmlns="urn:oasis:names:tc:xliff:document:2.0" srcLang="de">\n' +
+            '  <file original="ng.template" id="ngi18n">\n' +
+            '    <unit id="ID1">\n' +
+            '      <notes>\n' +
+            '        <note category="location">D:/Localization/Angular/Bugs/Icu/src/app/app.component.ts:2</note>\n' +
+            '      </notes>' +
+            '      <segment>\n' +
+            '        <source>source text</source>\n' +
+            '      </segment>\n' +
+            '    </unit>\n' +
+            '  </file>\n' +
+            '</xliff>';
+        const destFileContent = '<xliff version="2.0" xmlns="urn:oasis:names:tc:xliff:document:2.0" srcLang="de" trgLang="fr-CH">\n' +
+            '  <file original="ng.template" id="ngi18n">\n' +
+            '  </file>\n' +
+            '</xliff>';
+
+        const result = merge(sourceFileContent, destFileContent);
+
+        expect(norm(result)).toEqual(norm('<xliff version="2.0" xmlns="urn:oasis:names:tc:xliff:document:2.0" srcLang="de" trgLang="fr-CH">\n' +
+            '  <file original="ng.template" id="ngi18n">\n' +
+            '    <unit id="ID1">\n' +
+            '      <notes>\n' +
+            '        <note category="location">D:/Localization/Angular/Bugs/Icu/src/app/app.component.ts:2</note>\n' +
+            '      </notes>\n' +
+            '      <segment state="initial">\n' +
+            '        <source>source text</source>\n' +
+            '        <target>source text</target>\n' +
+            '      </segment>\n' +
+            '    </unit>\n' +
+            '  </file>\n' +
+            '</xliff>'));
     });
 });
+
+function norm(xml: string): string {
+    return new XmlDocument(xml).toString({compressed: true, preserveWhitespace: false});
+}
--- src/merge.ts
+++ src/merge.ts
@@ -5,7 +5,7 @@ import levenshtein from 'js-levenshtein';
 const FUZZY_THRESHOLD = 0.2;
 
 function getDestUnit(originUnit: XmlElement, destUnitsParent: XmlElement, removedNodes: XmlNode[]): XmlElement | undefined {
-    const destUnit = destUnitsParent.childWithAttribute('id', originUnit.attr['id']);
+    const destUnit = destUnitsParent.childWithAttribute('id', originUnit.attr.id);
     if (!destUnit) {
         const originText = toString(originUnit.childNamed('segment')?.childNamed('source')!);
         const closestUnit = removedNodes
@@ -40,13 +40,14 @@ export function merge(inFileContent: string, destFileContent: string, options?: 
     const destFileElement = destDoc.childNamed('file')!;
 
     // collect (potentially) obsolete units (defer actual removal to allow for fuzzy matching..):
-    const originIds = new Set(inFileElement.childrenNamed('unit').map(u => u.attr['id']));
-    let removeNodes = destFileElement.childrenNamed('unit').filter(destUnit => !originIds.has(destUnit.attr['id']));
+    const originIds = new Set(inFileElement.childrenNamed('unit').map(u => u.attr.id));
+    let removeNodes = destFileElement.childrenNamed('unit').filter(destUnit => !originIds.has(destUnit.attr.id));
 
     // add missing units:
     for (const unit of inFileElement.childrenNamed('unit') ?? []) {
         const destUnit = getDestUnit(unit, destFileElement, options?.fuzzyMatch ?? true ? removeNodes : []);
-        const unitSource = unit.childNamed('segment')!.childNamed('source')!;
+        const segmentSource = unit.childNamed('segment')!;
+        const unitSource = segmentSource.childNamed('source')!;
         const unitSourceText = toString(...unitSource.children);
         if (destUnit) {
             const destSource = destUnit.childNamed('segment')!.childNamed('source')!;
@@ -55,30 +56,25 @@ export function merge(inFileContent: string, destFileContent: string, options?: 
                 destSource.children = unitSource.children;
                 destSource.firstChild = destSource.children[0];
                 destSource.lastChild = destSource.children[destSource.children.length - 1];
-                destUnit.childNamed('segment')!.attr['state'] = 'initial'; // reset translation state, as source changed // TODO configurable?
-                console.debug(`update element with id "${unit.attr['id']}" with new source: ${toString(...destSource.children)} (was: ${destSourceText})`);
+                destUnit.childNamed('segment')!.attr.state = 'initial'; // reset translation state, as source changed // TODO configurable?
+                console.debug(`update element with id "${unit.attr.id}" with new source: ${toString(...destSource.children)} (was: ${destSourceText})`);
             }
-            if (destUnit.attr['id'] !== unit.attr['id']) {
-                console.debug(`matched unit with previous id "${destUnit.attr['id']}" to new id: "${unit.attr['id']}"`);
+            if (destUnit.attr.id !== unit.attr.id) {
+                console.debug(`matched unit with previous id "${destUnit.attr.id}" to new id: "${unit.attr.id}"`);
                 removeNodes = removeNodes.filter(n => n !== destUnit);
-                destUnit.attr['id'] = unit.attr['id'];
-                destUnit.childNamed('segment')!.attr['state'] = 'initial'; // reset translation state, as source changed // TODO configurable?
+                destUnit.attr.id = unit.attr.id;
+                destUnit.childNamed('segment')!.attr.state = 'initial'; // reset translation state, as source changed // TODO configurable?
             }
         } else {
-            console.debug(`adding element with id "${unit.attr['id']}"`);
-            // formatting assumes 2 spaces indentation:
-            const newElement = new XmlDocument(`<unit id="${unit.attr['id']}">
-      <segment state="initial">
-        <source>${unitSourceText}</source>
-        <target>${unitSourceText}</target>
-      </segment>
-    </unit>`);
-            destFileElement.children.push(newElement); // known: formatting not optimal (missing new line..)
+            console.debug(`adding element with id "${unit.attr.id}"`);
+            segmentSource.attr.state = 'initial';
+            segmentSource.children.push(new XmlDocument(`<target>${unitSourceText}</target>`))
+            destFileElement.children.push(unit);
             destFileElement.lastChild = destFileElement.children[destFileElement.children.length - 1];
         }
     }
 
-    console.debug(`removing ${removeNodes.length} ids: ${removeNodes.map(n => n.attr['id']).join(', ')}`);
+    console.debug(`removing ${removeNodes.length} ids: ${removeNodes.map(n => n.attr.id).join(', ')}`);
     removeChildren(destFileElement, ...removeNodes);
 
     // retain xml declaration:
@@ -97,9 +93,6 @@ export function merge(inFileContent: string, destFileContent: string, options?: 
 function removeChildren(node: XmlElement, ...children: XmlNode[]): void {
     const removeIndexes = new Set<number>(node.children.map((c, i) => children.indexOf(c) >= 0 ? i : null).filter(x => x !== null) as number[]);
     node.children = node.children.filter((c, i) => !removeIndexes.has(i) && (!removeIndexes.has(i + 1) || !isWhiteSpace(c)));
-    if (node.children.length === 1 && isWhiteSpace(node.children[0])) {
-        node.children = [];
-    }
     node.firstChild = node.children[0];
     node.lastChild = node.children[node.children.length - 1];
 }
